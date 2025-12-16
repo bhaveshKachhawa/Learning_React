317f976a23e85a350d28288e34b40a6451d5004b
Bhavesh Kachhawa <kachhawabhavesh27@gmail.com>
2025-12-16 20:09:30 +0530

    increase decrease button

---
+++ todo/increaseCount.js
@@ -0,0 +1,15 @@
+import {useState} from 'react';
+
+const App = () => {
+  const [value, setValue] = useState(0);
+  
+  return (
+    <div><button onClick={() => {
+      setValue(value+1);
+    }}> + </button> {value} <button onClick={() => {
+      setValue(value-1);
+    }}> - </button></div>
+    );
+}
+
+export default App;
