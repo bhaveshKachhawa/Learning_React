3eea482b49df41659088812563e54fc7a4358e50
Bhavesh Kachhawa <kachhawabhavesh27@gmail.com>
2026-01-19 15:12:33 +0530

    add verce file for hosting

--- FoodDelivery/food-delivery/src/utils/useRestaurentData.js
+++ FoodDelivery/food-delivery/src/utils/useRestaurentData.js
@@ -5,7 +5,7 @@ const useRestaurentData = () => {
     const [filterData, setFilterData] = useState([]);
 
     const fetchData = async() => {
-      const jsonData = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=24.607913&lng=73.7330753&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING');
+      const jsonData = await fetch('/dapi/restaurants/list/v5?lat=24.607913&lng=73.7330753&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING');
       const data = await jsonData.json();
       setRestaurant(data?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
       setFilterData(data?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
